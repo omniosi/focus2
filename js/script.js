777010d4be7eb4ed0068d24aa580dbf10276d25f
Ornette Coleman <omniosi@gmail.com>
2018-08-12 00:26:08 -0700

    updated code for single image pull. updated resume file

--- js/script.js
+++ js/script.js
@@ -27,10 +27,10 @@ var content = [
     "brand":"HARVEY NICHOLS"
     ,"title":"DIGITAL AD"
     ,"type":"Front-End Development & Responsive Design"
-    ,"image":[{
+    ,"image":{
       "src":"img/harvey-nichols_lipstick-stain-remover.jpg"
       ,"alt":"Harvey Nichols digital ad"
-    }]
+    }
     ,"story":"A personal project, and a personal challenge, to recreate a print ad as a responsive html page."
     ,"process":"Convert an image of a print design into responsive HTML and CSS. Publish online and save the working code on <a href='https://github.com/omniosi/hnichols-css-ad' target='_blank'>github</a>"
     ,"results":"Taking on this code challenge reinforced my love of responsive design and increased my personal confidence."
@@ -39,22 +39,10 @@ var content = [
     "brand":"pkboo"
     ,"title":"ecommerce site"
     ,"type":"Website Design & Front-End Development"
-    ,"art":[{
-      "elm":"source"
-      ,"src":"img/canon-nytimes-takeover-jamie-foxx.mp4"
-      ,"type":"video/mp4"
-      ,"alt":""
-    },{
-      "elm":"source"
-      ,"src":"img/canon-nytimes-takeover-jamie-foxx.webm"
-      ,"type":"video/webm"
-      ,"alt":""
-    },{
-      "elm":"img"
-      ,"src":"img/canon_imagina10n_takeover.jpg"
-      ,"type":""
-      ,"alt":"Canon Project Imagina10n takeover ad"
-    }]
+    ,"image":{
+      "src":"img/pkboo_site_home_600W.jpg"
+      ,"alt":"pkbookids.com homepage"
+    }
     ,"story":"Starting from the logo design for this stylish baby accessory brand, I was able to match a sense of fun with practically, which is exactly the brand ethos of the client. For this project, I created the logo and the site design, then coded the static site that was then passed to the Shopify/Magento developer to add the ecommerce code."
     ,"process":"Layout in Adobe InDesign with retouching in Photoshop. Convert approved designs into HTML and CSS. Handoff final coded layouts to back-end developer to incorporate Shopify ecommerce functionality."
     ,"results":"The fun and versatile logo inspired new uses on the physical product; from labels and stamps to plans of a stuffed, plush toy. The website design immediately led to sales and was featured in press for the company."
@@ -99,7 +87,9 @@ function articleBlock(props){
         return choose()
       }).join("")+'</video>'
     }
-    return '<img src="'+props.image.src+'" alt="'+props.image.alt+'">'
+    if(props.image){
+      return '<img src="'+props.image.src+'" alt="'+props.image.alt+'">'
+    }
   }
   return '<article role="article"><ul class="top"><li>'+visual()+'</li><li class="story"><h1>'+props.brand+'</h1><h2>'+props.title+'</h2><p>'+props.story+'</p></li></ul><ul class="details"><li><h3>'+props.type+'</h3></li><li class="process"><h4>PROCESS</h4><p>'+props.process+'</p></li><li class="results"><h4>RESULTS</h4><p>'+props.results+'</p></li></ul></article>'
 }
